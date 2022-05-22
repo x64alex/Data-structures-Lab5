46b0377946dc248d4855b3f4f837b09ac2a61116
911_Cantor_Alexandru <57806915+x64alex@users.noreply.github.com>
2022-05-22 19:38:38 +0300

    Started working on the hw

---
+++ SortedIndexedList/ListIterator.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "SortedIndexedList.h"
+#include <stack>
+
+//DO NOT CHANGE THIS PART
+class ListIterator{
+	friend class SortedIndexedList;
+private:
+    BSTNode* current;
+    std::stack<BSTNode*> stack;
+	const SortedIndexedList& list;
+	ListIterator(const SortedIndexedList& list);
+public:
+	void first();
+	void next();
+	bool valid() const;
+    TComp getCurrent() const;
+};
+
+
---
+++ SortedIndexedList/SortedIndexeList.cpp
@@ -0,0 +1,95 @@
+#include "ListIterator.h"
+#include "SortedIndexedList.h"
+#include <iostream>
+using namespace std;
+#include <exception>
+
+SortedIndexedList::SortedIndexedList(Relation r) {
+	this->nodes[0].elem = -1;
+    this->nodes[0].left = 1;
+    this->nodes[0].right = 2;
+    this->relation = r;
+}
+
+int SortedIndexedList::size() const {
+    if(this->isEmpty()) return 0;
+    BSTNode node = this->nodes[0];
+    int size =0;
+    while(node.elem !=-1){
+        size+= node.leftchildren+1;
+        node = this->nodes[node.right];
+    }
+
+    return size;
+}
+
+bool SortedIndexedList::isEmpty() const {
+	return this->nodes[0].elem== -1;
+}
+
+TComp SortedIndexedList::getElement(int i) const{
+	//TODO - Implementation get element
+	return NULL_TCOMP;
+}
+
+TComp SortedIndexedList::remove(int i) {
+	//TODO - Implementation remove
+	return NULL_TCOMP;
+}
+
+int SortedIndexedList::search(TComp e) const {
+    BSTNode node=this->nodes[0];
+    int index = 0;
+    while(node.elem!= -1){
+        if(node.elem==e){
+            return index;
+        }
+        if (relation(node.elem, e)){
+            index = node.right;
+        }
+        else {
+            index = node.left;
+            node.leftchildren++;
+        }
+    }
+    return -1;
+}
+
+void SortedIndexedList::add(TComp e) {
+    if(this->isEmpty())
+    {
+        this->nodes[0].elem = e;
+        this->nodes[0].left = 1;
+        this->nodes[0].right = 2;
+        this->nodes[1].elem = -1;
+        this->nodes[2].elem = -1;
+    }
+    else{
+        BSTNode node = this->nodes[0];
+        int index=0;
+        while(node.elem!=-1){
+            if (relation(node.elem, e)){
+                index = node.right;
+                node = this->nodes[index];
+            }
+            else {
+                index = node.left;
+                node.leftchildren++;
+                node = this->nodes[index];
+            }
+        }
+        node.elem = e;
+        node.left = index*2+1;
+        node.right = index*2+2;
+        this->nodes[node.left].elem = -1;
+        this->nodes[node.right].elem = -1;
+    }
+}
+
+ListIterator SortedIndexedList::iterator(){
+	return ListIterator(*this);
+}
+
+//destructor
+SortedIndexedList::~SortedIndexedList() {
+}
